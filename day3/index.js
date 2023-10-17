6660ae25db3cb63761b49fed4ae3fa1c2d45f2d3
ma278hdi <monday22220@gmail.com>
2023-10-17 16:58:46 +0300

    JS3 EX2

--- day3/index.js
+++ day3/index.js
@@ -1,5 +1,35 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res)=> res.json())
-    .then((data)=> console.log(data[1]))
 var canvas = document.querySelector('canvas')
-var c = canvas.getContext('2D')
+var width = 200
+var hight = 200
+var c = canvas.getContext('2d')
+
+
+c.fillStyle = 'blue'
+c.fillRect(0,0,width,hight)
+
+var cordnateX;
+var cordnateY;
+var pressed = false
+
+document.addEventListener('mousemove',e=>{
+    cordnateX = e.clientX
+    cordnateY = e.clientY
+
+})
+
+canvas.addEventListener('mousedown',()=> pressed = true)
+canvas.addEventListener('mouseup',()=> pressed= false)
+
+
+function drawLine(){
+    c.fillStyle = 'green'
+
+     if(pressed){
+        c.beginPath()
+        c.arc(cordnateX,cordnateY, 30 , 0 , 2*Math.PI ,false)
+        c.fill()
+     }
+     requestAnimationFrame(drawLine);
+
+}
+drawLine()
