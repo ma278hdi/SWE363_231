eda4f8513c58aa8719f8808053c995721a6abade
ma278hdi <monday22220@gmail.com>
2023-10-19 16:53:16 +0300

    JS4 EX1,2

--- day3/index.js
+++ day3/index.js
@@ -10,15 +10,20 @@ c.fillRect(0,0,width,hight)
 var cordnateX;
 var cordnateY;
 var pressed = false
-
-document.addEventListener('mousemove',e=>{
+const mousemoveEvent = function(e){
     cordnateX = e.clientX
     cordnateY = e.clientY
 
-})
+}
+document.addEventListener('mousemove',mousemoveEvent)
 
-canvas.addEventListener('mousedown',()=> pressed = true)
-canvas.addEventListener('mouseup',()=> pressed= false)
+
+canvas.addEventListener('mousedown',(async ()=>{
+    pressed = true
+})())
+canvas.addEventListener('mouseup',(function(){
+    pressed = false
+})())
 
 
 function drawLine(){
@@ -34,4 +39,18 @@ function drawLine(){
      c.stroke()
 
 }
-drawLine()
+drawLine() 
+
+class Coffee{
+    constructor(name,type){
+        this.name = name,
+        this.type = type
+    }
+}
+var coffeeObject = []
+var coffee1 = new Coffee("L.TONE","Bujuku")
+var coffee2 = new Coffee("roasting house","Colombia")
+console.log(typeof(coffee1))
+coffeeObject[0] = coffee1
+coffeeObject[1] = coffee2
+console.log(coffeeObject[1].name)
